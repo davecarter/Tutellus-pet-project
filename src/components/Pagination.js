e9730866831ae374f78b75351fc296b2a315bba8
David G <dave74@gmail.com>
2021-03-17 11:06:46 +0100

    adds handle increase and decrease pagination

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,11 +1,21 @@
-import React from "react"
-import { Link, useParams, useHistory } from "react-router-dom"
+import React, {useState} from "react"
+import { useParams, useHistory } from "react-router-dom"
 
 const className = "tmdbPagination"
 
-export const Pagination = ({ page, totalPages, totalResults }) => {
+export const Pagination = ({ totalPages, totalResults }) => {
+  const [isDisabled, setIsDisabled] = useState(true)
   const { pageNumber } = useParams()
   const history = useHistory()
+
+  const handleIncrease = () => {
+    setIsDisabled(false)
+    history.push(`${+pageNumber + 1}`)
+  }
+
+  const handleDecrease = () => {
+    pageNumber >= 2 ? history.push(`${+pageNumber - 1}`) : setIsDisabled(true)
+  }
 
   return (
     <section className={className}>
@@ -17,13 +27,13 @@ export const Pagination = ({ page, totalPages, totalResults }) => {
       </ul>
       <ul className={`${className}-controls`}>
         <li className={`${className}-controlsItem`}>
-          <button onClick={() => history.push(`${+pageNumber - 1}`)}>◄</button>
+          <button disabled={isDisabled} onClick={handleDecrease}>◄</button>
         </li>
         <li className={`${className}-controlsCurrent`}>
           current page: {pageNumber}
         </li>
         <li className={`${className}-controlsItem`}>
-          <button onClick={() => history.push(`${+pageNumber + 1}`)}>►</button>
+          <button onClick={handleIncrease}>►</button>
         </li>
       </ul>
     </section>
