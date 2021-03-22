259d55b5f264fc7394d47d370fe2f11360b2a98a
David G <dave74@gmail.com>
2021-03-22 19:00:09 +0100

    update test to throw InvalidMovieId custom error

--- src/test/GetMovieByIdUseCase.js
+++ src/test/GetMovieByIdUseCase.js
@@ -1,9 +1,14 @@
 import chai, { expect } from "chai"
+import chaiAsPromised from "chai-as-promised"
 import sinon from "sinon"
 import { AxiosFetcher } from "../domain/TMDB/Fetchers/AxiosFetcher"
 
 import { Domain } from "../domain"
+import { InvalidMovieId } from "../domain/TMDB/Errors/InvalidMovieId"
 import getMovieByIdUseCaseResponse from "./fixtures/getMovieByIdUseCaseResponse.json"
+
+// Allows try/catching async errors rejected from promises
+chai.use(chaiAsPromised)
 
 let domain
 let fetcher
@@ -22,9 +27,8 @@ describe("GetMovieByIdUseCase#execute", function () {
     stubedGet.restore()
   })
 
-  const movieId = "120"
-
   it("gets a movie by provided ID", async function () {
+    const movieId = "120"
     const resolved = new Promise((r) =>
       r({ DATA: getMovieByIdUseCaseResponse })
     )
@@ -32,5 +36,12 @@ describe("GetMovieByIdUseCase#execute", function () {
     const data = await domain.GetMovieByIdUseCase.execute({ movieId })
 
     expect(data).to.deep.equal(getMovieByIdUseCaseResponse)
+  })
+
+  it("throws an InvalidMovieId Error if provided MovieId has the wrong type", async function () {
+    const movieId = 120
+    await expect(
+      domain.GetMovieByIdUseCase.execute({ movieId })
+    ).to.be.rejectedWith(InvalidMovieId)
   })
 })
